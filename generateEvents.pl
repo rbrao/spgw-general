88bb0b39eabcaf31e1d89e001edba256976c0bb7
Ravishankar Rao <rbrao@Ravishankars-MBP.local>
2023-04-21 18:09:38 +0530

    full json generation complete

--- generateEvents.pl
+++ generateEvents.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/env perl
-#curl -ks --json '{"event": {"firstTime":"2023-04-21 04:04:04", "entity":"newTest", "severity":"WARNING", "eventType":"CREATE","alertSummary":"http alert","alertNotes":"something","eventSource":"Solarwinds","serviceName":"network service","eventID":"iu82jdsd822dksd","eventGroups":"OS","eventStatus":"OPEN"}}' https://43.205.54.81/events.json
 use strict;
-use Data::GUID qw(guid_string);
 use Data::Random qw(:all);
+use Getopt::Long;
+use JSON;
 
 my @severity = qw(WARNING INFO CRITICAL);
 my @eventType = qw(CREATE);
@@ -13,31 +13,55 @@ my @eventSource = qw(Solarwinds Nagios Zabbix Site24x7 PRTG Elasticsearch Logsta
 my @serviceName;
 my @eventGroups = qw(DB Linux Windows Network App1 App2 App3 Svc1 Svc2 Svc3 Storage Vmware AWS GCP Azure);
 
+my ($verbose, $usage, $url, $count);
+GetOptions ( 'v|verbose' => \$verbose, 'h|help' => \$usage, 'url:s' => \$url, 'count:s' => \$count );
+do { print "USAGE: $0 -url <url of event sources handler> -count <#of events to generate>\n"; exit(0) } if ( (defined $usage) || (not defined $url) || (not defined $count) );
+
 my $rand_ts = rand_datetime(min => '2023-01-01', max => 'now');
-my @entity = rand_words(size=>1);
-my @rand_severity = rand_set(set => \@severity, size => 1);
-my $eventID = rand_chars(set => 'alphanumeric', min => 12, max => 16);
+my @entity = rand_words(size => 1);
+my $rand_severity = rand_enum(set => \@severity);
+my $rand_eventType = rand_enum(set => \@eventType);
 my @alertSummary = rand_words(min => 2, max => 8);
 my @alertNotes = rand_words(min => 10, max => 25);
-my @rand_eventSource = rand_set(set => \@eventSource, size => 1);
+my $rand_eventSource = rand_enum(set => \@eventSource);
 my @serviceName = rand_words(min => 1, max => 3);
-my @rand_eventGroups = rand_set(set => \@eventGroups, size => 1);
+my $eventID = rand_chars(set => 'alphanumeric', min => 12, max => 16);
+my $rand_eventGroups = rand_enum(set => \@eventGroups);
+my $rand_eventStatus = rand_enum(set => \@eventStatus);
 
-print "firstTime: $rand_ts \
+print "
+firstTime: $rand_ts \
 entity: @entity \
-severity: @rand_severity \
-eventType: @eventType \
+severity: $rand_severity \
+eventType: $rand_eventType \
 alertSummary: @alertSummary \
 alertNotes: @alertNotes \
-eventSource: @rand_eventSource \
+eventSource: $rand_eventSource \
 serviceName: @serviceName \
 eventID: $eventID \
-eventGroups: @rand_eventGroups \
-eventStatus: @eventStatus \
-";
+eventGroups: $rand_eventGroups \
+eventStatus: $rand_eventStatus \
+" if (defined $verbose);
+
+my $json_string = JSON->new->utf8->encode( {
+	firstTime => $rand_ts,
+	entity => join(' ', @entity),
+	severity => $rand_severity,
+	eventType => $rand_eventType,
+	alertSummary => join(' ', @alertSummary),
+	alertNotes => join(' ', @alertNotes),
+	eventSource => $rand_eventSource,
+	serviceName => @serviceName,
+	eventID => $eventID,
+	eventGroups => $rand_eventGroups,
+	eventStatus => $rand_eventStatus
+} );
+print "{\"event\"\: $json_string }\n";
 
 __END__
+#curl -ks --json '{"event": {"firstTime":"2023-04-21 04:04:04", "entity":"newTest", "severity":"WARNING", "eventType":"CREATE","alertSummary":"http alert","alertNotes":"something","eventSource":"Solarwinds","serviceName":"network service","eventID":"iu82jdsd822dksd","eventGroups":"OS","eventStatus":"OPEN"}}' https://43.205.54.81/events.json
 #Backup routines
+use Data::GUID qw(guid_string);
 my @temp = localtime(time);
 my $ts = sprintf ( "%04d-%02d-%02d %02d:%02d:%02d", $temp[5]+1900,$temp[4]+1,$temp[3],$temp[2],$temp[1],$temp[0]);
 my $eventID = guid_string();
